e6b26b34d313fa686ce9a5dc786053fd315dfe01
Salvador <salvaadr0@gmail.com>
2026-01-12 11:13:48 +0100

    home controller y index.html modificados

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,7 @@ import './stimulus_bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
